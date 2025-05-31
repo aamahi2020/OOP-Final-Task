1631c0c6a505c5bf43c22112758a4d48991a6f3a
aamahi2020 <aamahi2020@gmail.com>
2025-05-31 19:27:37 +0300

    modified Wordindexentry.cs

--- Models/WordIndexEntry.cs
+++ Models/WordIndexEntry.cs
@@ -1,9 +1,9 @@
-namespace C__Final.Models
+namespace FileIndexingSystem.Models
 {
     public class WordIndexEntry
     {
-        public string FileName { get; set; }
-        public string Word { get; set; }
+        public string FileName { get; set; } = string.Empty;
+        public string Word { get; set; } = string.Empty;
         public int Count { get; set; }
     }
 }
